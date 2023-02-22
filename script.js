94d12284e9d12bad8c54706b65ec32e33b322163
csmit195 <csmit195@DESKTOP-30EPM17>
2023-02-23 09:37:11 +1000

    uh a lot of things?

--- script.js
+++ script.js
@@ -8,19 +8,61 @@ apiTokenField.addEventListener('input', () => {
     localStorage.setItem('apiToken', apiTokenField.value);
 });
 
+function validate(){
+    var apiToken = document.getElementById('api-token-field').value;
+    var steamId = document.getElementById('steamid-field').value;
+    
+    if (apiToken == "" || steamId == ""){
+        alert("Please fill in all fields.");
+        return false;
+    }
+
+    if ( apiToken.length != 29 ) {
+        alert("API Token is invalid.");
+        return false;
+    }
+
+    if ( steamId.length != 17 ) {
+        alert("SteamID is invalid.");
+        return false;
+    }
+
+    return true;
+}
+
 // Handle form submission
 const form = document.getElementById('disconnect-form');
 form.addEventListener('submit', async (event) => {
     event.preventDefault();
+
+    if (!validate()) {
+        return;
+    }
+
     const steamIdField = document.getElementById('steamid-field');
     const apiToken = apiTokenField.value;
     const steamId = steamIdField.value;
-    const url = `https://cors-anywhere.herokuapp.com/https://api.counter-strike.me/disconnect?steamid=${steamId}&key=${apiToken}`;
+
+    const submitButton = document.querySelector('#disconnect-form > button');
+    submitButton.disabled = true;
+    submitButton.classList.add('disabled');
+
+    const url = `https://api.counter-strike.me/disconnect?steamid=${steamId}&key=${apiToken}`;
     const response = await fetch(url);
     const jsonResponse = await response.json();
-    if (jsonResponse.error) {
-        alert(`Error: ${jsonResponse.error}`);
+
+    submitButton.disabled = false;
+    submitButton.classList.remove('disabled');
+
+    // if response 429, then the user has been rate limited, tell them to wait 1 minute
+    if (response.status === 429) {
+        alert('You have been rate limited. Please wait 10s-1m before trying again.');
+        return;
+    }
+
+    if (jsonResponse.success) {
+        alert(`Success! Disconnected from server with ID ${jsonResponse.server}`);
     } else {
-        alert(`Disconnected from server with ID ${jsonResponse.server}`);
+        alert(`Error: ${jsonResponse.error}`);
     }
 });
